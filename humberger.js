11c0714aa954cf5a3f62bb96f4a2c6b836d54740
touki-1513 <54700222+touki-1513@users.noreply.github.com>
2023-08-16 21:58:43 +0900

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   humberger.js
    	new file:   img/favicon.png
    	upd file:   index.html
    	new file:   jquery-3.5.1.min.js
    	new file:   sanitize.css
    	upd file:   stylesheet.css

---
+++ humberger.js
@@ -0,0 +1,11 @@
+// ハンバーガー
+$(function () {
+  $(".headerR").click(function () {
+    $(".headerBack").slideToggle();
+  });
+});
+$(function () {
+  $("#tojiru").click(function () {
+    $(".headerBack").slideToggle();
+  });
+});
